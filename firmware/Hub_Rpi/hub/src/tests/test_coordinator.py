d11dc3fd94be7aa730807a6ae8cb22972f8c1b29
SteveMayze <smayze@yahoo.com>
2021-04-24 18:06:58 +0200

    adds an initial check for exceptions

--- firmware/Hub_Rpi/hub/src/tests/test_coordinator.py
+++ firmware/Hub_Rpi/hub/src/tests/test_coordinator.py
@@ -2,53 +2,23 @@
 from leawood.config import Config
 from leawood.xbee import Coordinator
 from unittest.mock import patch
-from digi.xbee.devices import AbstractXBeeDevice
-from digi.xbee.devices import XBeeNetwork
-from digi.xbee.devices import XBeeDevice
+from digi.xbee.devices import XBeeException
 import pytest
 import json
 import leawood.xbee
+import tests.common
 
 @pytest.fixture
 def config():
     args = ["--serial-port", "COM1", "--baud", "9600"]
     return Config(args)
 
-class FakeCoordinatingDevice(XBeeDevice):
-    def __init__(self, com, baud):
-        XBeeDevice.__init__(self, com, baud)
-        self.spy = {}
-        self.log.info('FakeCoordinatingDevice: __init__')
-
-    def __str__(self):
-        return 'FakeCoordinatingDevice'
-
-
-class FakeCoordinator(Coordinator):
-    def __init__(self, config):
-        Coordinator.__init__(self, config )
-        com = config.config_data['serial-port']
-        baud = int(config.config_data['serial-baud'])
-        self.coordinating_device = FakeCoordinatingDevice(com, baud)
-        self.log.info('FakeCoordinator: __init__')
-
-    def _scan_network(self):
-        device = json.loads(f'{{"NI": "GREEN", "PL": "PowerLevel.High", "ADDRESS": "00000001", "ADDR": "0001"}}')
-        device['device-id'] = 'NOT-SET'
-        self.add_node(device)
-
-    def _request_data(self):
-        self.coordinating_device.spy['COORDINATOR'] = 'DATA_REQ'
-
-    def __str__(self):
-        return 'FakeCoordinator'
-
 
 
 class TestCase:
 
     def test_coordinator_scan_network(self, config):
-        coordinator = FakeCoordinator(config)
+        coordinator = tests.common.FakeCoordinator(config)
         status = leawood.xbee.scan_network(coordinator)
         assert "OK" == status
 
@@ -58,7 +28,7 @@ class TestCase:
 
 
     def test_coordinator_can_request_data(self, config):
-        coordinator = FakeCoordinator(config)
+        coordinator = tests.common.FakeCoordinator(config)
         device = json.loads(f'{{"NI": "GREEN", "PL": "FF", "ADDRESS": "00000001", "ADDR": "0001"}}')
         device['device-id'] = 'NOT-SET'
         coordinator.add_node(device)
@@ -69,5 +39,25 @@ class TestCase:
         # assert node_data[0]['voltage'] == 10.6
         assert coordinator.coordinating_device.spy["COORDINATOR"] == 'DATA_REQ'
 
+
+    """
+    Defines its own coordinator to throw an un qualified XBee Exception.
+    """
+    def test_xbee_exception_returns_failed(self, config):
+        ## A local fake coordinator just to throw an exception
+        class BrokenCoordinator(tests.common.FakeCoordinator):
+            def __init__(self, config):
+                tests.common.FakeCoordinator.__init__(self, config )
+                self.log.info('BrokenCoordinator: __init__')
+
+            def _scan_network(self):
+                raise XBeeException()
+
+        ## TODO - Further possibilities are here to examine the type
+        ##        of exception and produce a better response and handling
+        ##        i.e. logging the issues back to the base.
+        coordinator = BrokenCoordinator(config)
+        status = leawood.xbee.scan_network(coordinator)
+        assert "EXCEPTION" == status
 
 
